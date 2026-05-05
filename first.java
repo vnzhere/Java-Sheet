c906522fa81a66bff0dfe3a6e7e4f4001899c4a5
Vanzhere <vaniya.sharma158@gmail.com>
2026-05-05 08:26:11 +0000

    Initial Commit

--- first.java
+++ first.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class first{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+
+        // // What does args do?
+        // System.out.println(args[0]);
+    //    java first.java hello
+        // hello in terminal line got saved in args and then printed along with hello world
+//         System.out.println("Hello World!");
+// // my name is --0,1,2
+//          System.out.println(args[0]);
+//           System.out.println(args[1]);
+//            System.out.println(args[2]);
+
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+// we can give floating point input also 
+System.out.println("Hello People " + a);
+
+
     }
 }
