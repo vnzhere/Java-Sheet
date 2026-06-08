2d8d14bdba62da4f8d8820389390cc780e11ca4f
Vanzhere <vaniya.sharma158@gmail.com>
2026-06-08 12:25:55 +0000

    Initial Commit

--- list.java
+++ list.java
@@ -7,13 +7,21 @@ public class list{
         list.add(12);
         list.add(5);
         list.add(24);
-        list.add(12);
+        list.add(10);
         list.add(14);
 
 // list value update at index 1 to 26
-        list.set(1,26);
+        // list.set(1,26);
 
 // gets value as per index input 
         System.out.println(list.get(1));
+// remove 5 from the list
+        list.remove(1);
+
+// iterates from 12-14 all elements
+        for (int x : list){
+            System.out.println(x);
+        }
+
     }
 }
