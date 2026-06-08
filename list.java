f126636976fca8935d3a7342f07fb63e7e6e2e7b
Vanzhere <vaniya.sharma158@gmail.com>
2026-06-08 12:33:08 +0000

    initial Commit

--- list.java
+++ list.java
@@ -14,14 +14,18 @@ public class list{
         // list.set(1,26);
 
 // gets value as per index input 
-        System.out.println(list.get(1));
+        // System.out.println(list.get(1));
+
 // remove 5 from the list
-        list.remove(1);
+        // list.remove(1);
+        list.remove(Integer.valueOf(24));
 
 // iterates from 12-14 all elements
         for (int x : list){
             System.out.println(x);
         }
+        // find the total size of list
+        System.out.println(list.size());
 
     }
 }
